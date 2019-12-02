1dd010300d7c2aa0a4afd2aaa25e8cf8316394ca
Fihoca07 <Fihoca07@SINENSIA.EDU>
2019-12-02 16:47:39 +0100

    alta producto

--- src/app/components/productos/alta-producto/alta-producto.component.ts
+++ src/app/components/productos/alta-producto/alta-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-alta-producto',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltaProductoComponent implements OnInit {
 
-  constructor() { }
+  producto:Producto = new Producto();
+
+  constructor(private productoService:ProductoService) { }
 
   ngOnInit() {
+  }
+
+  crear(){
+    this.productoService.create(this.producto).subscribe(data => console.log(data));
   }
 
 }
